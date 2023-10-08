81efb4148ecb91ec2e37d54c7e369f58d2ab7cff
Vaughn Saben <vsaben99@gmail.com>
2023-10-08 17:04:42 +0300

    Updated projects and contact details.

---
+++ php/submit_form.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_POST['email'])) {
+    $email_to = "vsaben99@gmail.com";
+    $name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
+    $subject = $_POST['subject']; // required
+    $message = $_POST['message']; // required
+
+    function clean_string($string) {
+    $bad = array("content-type","bcc:","to:","cc:","href");
+    return str_replace($bad,"",$string);
+    }
+
+    $email_message = "Form details below.\n\n";
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Subject: ".clean_string($subject)."\n";
+    $email_message .= "Message: ".clean_string($message)."\n";
+
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+mail($email_to, $subject, $email_message, $headers);
+?>
+  <div class="feedback">Thank you for reaching out. If relevant, I will be in touch with you soon.</div>
+  <?php
+}
+?>
